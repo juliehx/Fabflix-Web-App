a73c03c0a03fefaf55c5e7eff9c2b6330003910d
Julie Ho <julieho.98@gmail.com>
2019-01-28 11:43:48 -0800

    added search and browse urls

---
+++ project1/src/IndexServlet.java
@@ -0,0 +1,84 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet(name = "IndexServlet", urlPatterns="/api/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Resource(name = "jdbc/moviedb")
+	private DataSource dataSource;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		
+		PrintWriter out = response.getWriter();
+		
+		try {
+			Connection dbcon = dataSource.getConnection();
+			Statement statement = dbcon.createStatement();
+			
+			String query = "select genres.id, genres.name from genres";
+			
+			ResultSet rs = statement.executeQuery(query);
+			
+			JsonArray jsonArray = new JsonArray();
+			
+			while(rs.next()) {
+				String id = rs.getString("id");
+				String genre = rs.getString("name");
+				
+				JsonObject jsonObject = new JsonObject();
+				jsonObject.addProperty("id", id);
+				jsonObject.addProperty("genre", genre);
+				
+				jsonArray.add(jsonObject);
+			}
+			
+			out.write(jsonArray.toString());
+			response.setStatus(200);
+			rs.close();
+			statement.close();
+			dbcon.close();
+		}
+		catch (Exception e) {
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("errorMessage", e.getMessage());
+			out.write(jsonObject.toString());
+			
+			response.setStatus(500);
+		}
+		out.close();
+		
+	}
+}
