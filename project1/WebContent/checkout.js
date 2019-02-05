392d5fa793ab3515542513e4b02f23234dec8a3b
Julie Ho <julieho.98@gmail.com>
2019-02-04 20:47:49 -0800

    added movie quantity to confirmation message

--- project1/WebContent/checkout.js
+++ project1/WebContent/checkout.js
@@ -2,7 +2,7 @@ function handleResult(data) {
 	console.log(data);
 	$('.message').html("<p>" + data["message"] + "</p>");
 	if('status' in data && data["status"] == "success") {
-		$('.message').append("<p>Product Details: </p><ul></ul>");
+		$('.message').append("<p>Product Details: </p><p>" + "Purchased " +  data["sales"].length + " movies</p><ul></ul>");
 		for(let i = 0; i < data["sales"].length; i++) {
 			$('.message ul').append("<li>" + data["sales"][i]["sale_id"] + ": <a href='single-movie.html?id=" + data["sales"][i]["movie_id"] + "'> "+ data["sales"][i]["title"] + "</a></li>");
 		}
