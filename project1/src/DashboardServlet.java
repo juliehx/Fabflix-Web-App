fa749976ccb0051c52813cb6d0796cac7432b185
Chad Huang <chadkh@uci.edu>
2019-02-11 14:03:39 -0800

    added dashboard file

---
+++ project1/src/DashboardServlet.java
@@ -0,0 +1,53 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.sql.ResultSet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+@WebServlet(name = "DashboardServlet", urlPatterns = "/api/_dashboard")
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+      
+	@Resource(name = "jdbc/moviedb")
+	private DataSource dataSource;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		
+	}
+
+
+}
