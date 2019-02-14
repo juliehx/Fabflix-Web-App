6c13d8dba9c714b8f0b1db518ac0badf03472c83
Julie Ho <julieho.98@gmail.com>
2019-02-13 18:07:44 -0800

    forms for adding a star

--- project1/WebContent/__dashboard.js
+++ project1/WebContent/__dashboard.js
@@ -2,9 +2,20 @@ function handleResult(result) {
 	console.log(result);
 }
 
-$.ajax({
-	dataType: 'json',
-	method: 'GET',
-	url: 'api/_dashboard',
-	success: (result) => handleResult(result)
-});
+function handleAddStarCallback(data) {
+	$("#message").text(data["message"]);
+}
+
+function handleAddStar(event) {
+	event.preventDefault();
+	$.get("api/add-star", $("#add-star-form").serialize(), (data)=>handleAddStarCallback(data));
+}
+
+$("#add-star-form").on("submit", (event)=>handleAddStar(event));
+
+//$.ajax({
+//	dataType: 'json',
+//	method: 'GET',
+//	url: 'api/_dashboard',
+//	success: (result) => handleResult(result)
+//});
