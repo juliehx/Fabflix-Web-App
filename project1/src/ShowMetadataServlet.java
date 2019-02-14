834f1d677074719e86fcf80ec518072e7e33ccda
Chad Huang <chadkh@uci.edu>
2019-02-14 01:02:41 -0800

    added showMetadata servlet file

---
+++ project1/src/ShowMetadataServlet.java
@@ -0,0 +1,102 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.sql.ResultSet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowMetadataServlet
+ */
+@WebServlet(name = "ShowMetadataServlet", urlPatterns = "/api/show-metadata")
+public class ShowMetadataServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@Resource(name="jdbc/moviedb")
+	private DataSource dataSource;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowMetadataServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		String dbName = "moviedb";
+		
+		try {
+			
+			Connection dbcon = dataSource.getConnection();
+			
+			DatabaseMetaData meta = dbcon.getMetaData();
+			
+			String[] types = {"TABLE"};
+			ResultSet tables = meta.getTables(dbName,null, "%", types);
+			
+			HashMap <String,HashMap<String,String>> metaData = new HashMap<String, HashMap<String,String>>();
+			
+			while(tables.next()) {
+				
+				String table_name = tables.getString("TABLE_NAME");
+				ResultSet columns = meta.getColumns(dbName,null, table_name, "%");//get column name and type of column
+				HashMap <String,String> column_info = new HashMap<String,String>();
+				
+				while(columns.next()) {
+					
+					String column_name = columns.getString("COLUMN_NAME");
+					String data_type = columns.getString("COLUMN_SIZE");
+					column_info.put(column_name, data_type);
+					
+				}
+				
+				columns.close();
+				metaData.put(table_name, column_info);		
+				
+			}
+			
+			Gson gson = new Gson();
+			String data = gson.toJson(metaData);
+			response.getWriter().write(data.toString());
+			
+			response.setStatus(200);
+			
+			tables.close();
+			dbcon.close();
+			
+		}catch (Exception e) {
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("status", "Fail");
+			jsonObject.addProperty("message", "Failed to retrieve Database Metadata.");
+			response.getWriter().write(jsonObject.toString());
+		}
+	}
+
+}
