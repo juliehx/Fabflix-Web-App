232e6eb633da651bf71b425f3b40dd6554c4710d
Julie Ho <julieho.98@gmail.com>
2019-02-25 19:36:43 -0800

    autocomplete

---
+++ project1/WebContent/autocomplete.js
@@ -0,0 +1,41 @@
+function handleLookup(query, doneCallback) {
+	console.log("start autocomplete")
+	
+	$.ajax({
+		datatype: "json",
+		method: "GET",
+		url: "api/autocomplete-suggestion?query=" + escape(query),
+		success: function(data) {
+			handleLookupAjaxSuccess(data, query, doneCallback)
+		},
+		error: function(errorData) {
+			console.log("lookup ajax error")
+			console.log(errorData)
+		}
+	})
+}
+
+function handleLookupAjaxSuccess(data, query, doneCallback) {
+	console.log("lookup ajax successful")
+	console.log(data)
+	
+//	var jsonData = JSON.parse(data);
+//	console.log(jsonData)
+	
+	doneCallback({suggestions:data});
+}
+
+function handleSelectSuggestion(suggestion) {
+	window.location.replace("single-movie.html?id=" + suggestion["data"]["id"])
+}
+
+$("#autocomplete").autocomplete({
+	lookup: function(query, doneCallback) {
+		handleLookup(query, doneCallback)
+	},
+	onSelect: function(suggestion) {
+		handleSelectSuggestion(suggestion)
+	},
+	deferRequestBy: 300,
+	minChars: 3,
+});
---
+++ project1/src/AutocompleteServlet.java
@@ -0,0 +1,105 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class AutocompleteServlet
+ */
+@WebServlet(name="AutocompleteServlet", urlPatterns="/api/autocomplete-suggestion")
+public class AutocompleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AutocompleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Resource(name = "jdbc/moviedb")
+    private DataSource dataSource;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		
+		PrintWriter out = response.getWriter();
+		
+		try {
+			JsonArray jsonArray = new JsonArray();
+			
+			String title = request.getParameter("query");
+			
+			if (title == null || title.trim().isEmpty()) {
+				out.write(jsonArray.toString());
+				return;
+			}
+			
+			Connection dbcon = dataSource.getConnection();
+			
+			String query = String.format("select movies.id, movies.title from movies where match(movies.title) against('%s' in boolean mode) limit 10\n", title);
+			
+			PreparedStatement statement = dbcon.prepareStatement(query);
+			
+			ResultSet rs = statement.executeQuery();
+			
+			while (rs.next()) {
+				String movieTitle = rs.getString("title");
+				String movieId = rs.getString("id");
+				
+				JsonObject jsonObject = new JsonObject();
+				jsonObject.addProperty("value", movieTitle);
+				
+				JsonObject jsonData = new JsonObject();
+				jsonData.addProperty("id", movieId);
+				
+				jsonObject.add("data", jsonData);
+				
+				jsonArray.add(jsonObject);
+			}
+			
+			out.write(jsonArray.toString());
+			
+			response.setStatus(200);
+			rs.close();
+			statement.close();
+			dbcon.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			JsonObject jsonObject = new JsonObject();
+			jsonObject.addProperty("errorMessage", e.getMessage());
+			
+			response.setStatus(500);
+		}
+		
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+}
