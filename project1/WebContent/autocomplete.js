02fe0a7a10186c026ea4386dc077d36ba146e947
Chad Huang <chadkh@uci.edu>
2019-02-26 19:09:33 -0800

    added caching to search results

--- project1/WebContent/autocomplete.js
+++ project1/WebContent/autocomplete.js
@@ -1,26 +1,39 @@
+var lookupCache = {};
+
+
 function handleLookup(query, doneCallback) {
 	console.log("start autocomplete")
 	
-	$.ajax({
-		datatype: "json",
-		method: "GET",
-		url: "api/autocomplete-suggestion?query=" + escape(query),
-		success: function(data) {
-			handleLookupAjaxSuccess(data, query, doneCallback)
-		},
-		error: function(errorData) {
-			console.log("lookup ajax error")
-			console.log(errorData)
-		}
-	})
+	if(query in lookupCache){
+		
+		console.log("lookup cache successful")
+		console.log(lookupCache[query])
+		doneCallback({suggestions:lookupCache[query]});
+	}
+	else{
+		$.ajax({
+			datatype: "json",
+			method: "GET",
+			url: "api/autocomplete-suggestion?query=" + escape(query),
+			success: function(data) {
+				handleLookupAjaxSuccess(data, query, doneCallback)
+			},
+			error: function(errorData) {
+				console.log("lookup ajax error")
+				console.log(errorData)
+			}
+		})
+	}
 }
 
 function handleLookupAjaxSuccess(data, query, doneCallback) {
 	console.log("lookup ajax successful")
 	console.log(data)
 	
-//	var jsonData = JSON.parse(data);
-//	console.log(jsonData)
+	if(!(query in lookupCache)){
+		lookupCache[query] = data;
+	}
+	//check past query results here
 	
 	doneCallback({suggestions:data});
 }
@@ -28,6 +41,8 @@ function handleLookupAjaxSuccess(data, query, doneCallback) {
 function handleSelectSuggestion(suggestion) {
 	window.location.replace("single-movie.html?id=" + suggestion["data"]["id"])
 }
+
+
 
 $("#autocomplete").autocomplete({
 	lookup: function(query, doneCallback) {
@@ -39,3 +54,12 @@ $("#autocomplete").autocomplete({
 	deferRequestBy: 300,
 	minChars: 4,
 });
+
+var time = setInterval(myTimer,600000);
+
+function myTimer(){
+	console.log("clearing cache...")
+	lookupCache = {};
+	console.log("cache cleared!")
+	
+}
